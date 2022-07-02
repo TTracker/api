b9bd85140d9951f5f96f978445186b9f4504ccc2
mondrejcak <64928180+theadam-exe@users.noreply.github.com>
2022-07-02 13:00:10 +0200

    📍 Deprecated error_handler.php

--- utils/error_handler.php
+++ utils/error_handler.php
@@ -15,6 +15,7 @@
   * @license    http://www.gnu.org/licenses/gpl-3.0.html  GNU GPL v3
   * @version    prerelease-1.0
   * @since      File available since alpha-2.1.1
+  * @deprecated File deprecated since beta-1.0
   */
 
   /**
@@ -26,6 +27,9 @@
    * @param string $eline  Line where the error occurred
    * 
    * @author Adam Ondrejčák <adam.ondrejcak@gmail.com>
+   * 
+   * @since       Function available since alpha-2.1.1
+   * @deprecated  Function deprecated since beta-1.0
    */
   function error_handler($etype, $emsg, $efile, $eline) {
 
